31d1ffd3d9b03564430f3ed9ef47851b621e8c99
HTinh123 <bijosir@gmail.com>
2026-07-24 16:44:25 +0700

    c

--- server/index.js
+++ server/index.js
@@ -67,9 +67,9 @@ app.get('/api/health', async (req, res) => {
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-    });
+   app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
 }
 
 app.listen(PORT, '0.0.0.0', () => {
